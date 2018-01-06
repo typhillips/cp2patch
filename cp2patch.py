24795eb04c30fd30e2d4ddd39de3947a80bd525f
Ty Phillips <typhillips73@gmail.com>
2018-01-05 22:41:19 -0600

    Rough framework for the library established. Args established and rough class design, but nothing actually implemented yet.

---
+++ cp2patch.py
@@ -0,0 +1,71 @@
+
+# cp2patch
+#
+# Copyright 2017 Ty Phillips. All Rights Reserved.
+# This program is free software. You can redistribute and/or modify it in
+# accordance with the terms of the accompanying license agreement.
+
+#!/usr/bin/env python
+
+import sys
+import os
+import difflib
+import argparse
+
+class CP2Patch(object):
+	def __init__(self, cpnum, hostname=None, username=None, password=None, destination=None):
+		self.hostname = hostname
+		self.username = username
+		self.password = password
+		self.destination = destination
+		self.cpnum = cpnum
+
+	def make_patch(self):
+		cpinfo = self.get_cpinfo()
+
+		for filename in cpinfo:
+			#TODO
+			# Run 'si viewrevision' to dump old and new revisions to
+			#   new files
+			# Use difflib.unified_diff() to generate patch files
+			# Somehow come up with a way to generate an error (and/or skip patch file creation) if patch fails
+			pass
+
+
+
+	def get_cpinfo(self):
+		#TODO
+		# Run 'si viewcp' to get list of files and version numbers
+		# For each file
+		#   Run 'si rlog' on each file
+		#   Search output to find version number and previous version number
+		#   Add this to the running list
+		# Return list of files with new and old version numbers
+		return []
+
+
+
+class ShellRun(object):
+	def __init__(self):
+		parser = argparse.ArgumentParser(description="Create PATCH files from Integrity change package.")
+		parser.add_argument("-n", "--hostname", help="Integrity host name")
+		parser.add_argument("-u", "--username", help="Integrity user name")
+		parser.add_argument("-p", "--password", help="Integrity password")
+		parser.add_argument("-d", "--destination", help="destination path for files")
+		parser.add_argument("cp", type=int, help="change package number")
+		args = parser.parse_args()
+
+		self.hostname = args.hostname
+		self.username = args.username
+		self.password = args.password
+		self.destination = args.destination
+		self.cpnum = args.cp
+
+	def run(self):
+		cp2patch = CP2Patch(self.cpnum, hostname=self.hostname, password=self.password, destination=self.destination)
+		cp2patch.make_patch()
+
+
+if __name__ == "__main__":
+	app = ShellRun()
+	app.run()
